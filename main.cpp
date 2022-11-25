d804181452915b58d65a0d8e65a90247a52c39b1
PatrykGniota <>
2022-11-25 13:09:37 +0000

    Palindrom v 2

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
 
-  int n =1221;
+  int n = 1221;
   string word = to_string(n);
   bool isPalindrom = true;
 
-  for(int i = 0; i < word.size() / 2 ; i++){
-    if(word[i] == word[word.size() -(i+1)]){
+  for(int i = 0, k = word.size()-1; i < k ; i++,k--){
+    if(word[i] == word[k]){
       isPalindrom = true;
     }
     else{
