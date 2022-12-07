d9d4d18937a2ae02e60a074d2b5d55e8d79d5024
PatrykGniota <>
2022-12-07 08:56:02 +0000

    start

--- .ccls-cache/@home@runner@4Czy-to-jest-palindrom/main.cpp
+++ .ccls-cache/@home@runner@4Czy-to-jest-palindrom/main.cpp
@@ -3,24 +3,6 @@ using namespace std;
 
 int main() {
 
-  int n = 1221;
-  string word = to_string(n);
-  bool isPalindrom = true;
-
-  for(int i = 0, k = word.size()-1; i < k ; i++,k--){
-    if(word[i] == word[k]){
-      isPalindrom = true;
-    }
-    else{
-      isPalindrom = false;
-      break;
-    }
-  }
   
-  if(isPalindrom){
-    cout << "To jest palindrom" << '\n';
-  }
-  else{
-    cout << "To nie jest palindrom" << '\n';
-  }
+  
 }
