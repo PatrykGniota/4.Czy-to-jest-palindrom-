5b1385630a0cbaeaec353dedd6a53423e448d5d2
PatrykGniota <120011921+PatrykGniota@users.noreply.github.com>
2022-12-12 10:47:10 +0000

    working without function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
+#include<cctype>
 using namespace std;
+
+//isalpha() czy jest alfanumeryczny
+//isspace() czy jest spacją;
+
 
 int main() {
 
-  
-  
+  string s = "Ala ma kota, a kot ma Ale";
+  string temp = "";
+  string word = "";
+  bool isPalindrom = true;
+
+  for(int i = 0; i < s.size(); i++){
+    s[i] = toupper(s[i]);
+  }
+
+  for(int i = 0; i < s.size(); i++){
+    if((s[i] >= 'A' && s[i] <= 'Z') || isspace(s[i])){
+      temp += s[i];      
+    }
+   }
+  for(int j = 0; j < temp.size(); j++){
+    if(not isspace(temp[j])){
+    word += temp[j];
+    }
+    else{
+    for(int i = 0, k = word.size()-1; i <= k ; i++,k--)
+    {
+      if(word[i] == word[k]){
+        isPalindrom = true;
+      }
+      else{
+        isPalindrom = false;
+        break;
+      }
+    }
+    cout << word + ' ';
+    if(isPalindrom){
+      cout << "Palindrom\n";
+      word = "";
+    }
+    else{
+      cout << "Not Palindrom\n";
+      word = "";
+    }
+
+    }
+  }
 }
