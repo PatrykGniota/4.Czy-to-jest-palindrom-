0b40d37674d62a7f4a0661d4c2d39d8a15a45db8
PatrykGniota <120011921+PatrykGniota@users.noreply.github.com>
2022-12-14 09:12:26 +0000

    Funkcje z podręcznika

--- main.cpp
+++ main.cpp
@@ -11,50 +11,12 @@ int main() {
   //Deklaracja zmiennych
   
   string s = "Ala ma kota, a kot ma Ale "; //Notka: dodać spacje po ostatnim wyrazie
-  string temp = "";
-  string word = "";
-  bool isPalindrom = true;
-  
-  //Zamiana na Wielkie litery
-  
-  for(int i = 0; i < s.size(); i++){
-    s[i] = toupper(s[i]);
-  }
 
-  //Usuwanie zbędnych znaków
-  
-  for(int i = 0; i < s.size(); i++){
-    if((s[i] >= 'A' && s[i] <= 'Z') || isspace(s[i])){
-      temp += s[i];      
-    }
+  while(s.size()){
+    int space = s.find(' ');
+    string word = s.substr(0,space);
+    cout << word <<endl; // isPalindro,
+    s.erase(0,space + 1);
   }
-
-  //Dzielenie na słowa i sprawdzanie palindromowości
   
-  for(int j = 0; j < temp.size(); j++){
-    if(not isspace(temp[j])){
-    word += temp[j];
-    }
-    else{
-    for(int i = 0, k = word.size()-1; i <= k ; i++,k--)
-    {
-      if(word[i] == word[k]){
-        isPalindrom = true;
-      }
-      else{
-        isPalindrom = false;
-        break;
-      }
-    }
-    cout << word + ' ';
-    if(isPalindrom){
-      cout << "Palindrom\n";
-      word = "";
-    }
-    else{
-      cout << "Not Palindrom\n";
-      word = "";
-    }
-    }
-  }
 }
